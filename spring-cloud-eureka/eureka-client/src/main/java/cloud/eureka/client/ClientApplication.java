c2772c0fc1866bba71dc8408621bf87f9d31bc76
booxj <1416890997@qq.com>
2018-11-15 13:19:43 +0800

    spring-cloud-eureka

---
+++ spring-cloud-eureka/eureka-client/src/main/java/cloud/eureka/client/ClientApplication.java
@@ -0,0 +1,27 @@
+package cloud.eureka.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public String home(@RequestParam(value = "name", defaultValue = "booxj") String name) {
+        return "hello " + name + " ,i am from port:" + port;
+    }
+}
